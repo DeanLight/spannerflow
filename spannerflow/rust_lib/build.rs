a3fb151a9d184756bf76726485e34e7f1d20ade6
Ofer Katz <ofer.katz@campus.technion.ac.il>
2025-02-22 17:27:09 +0200

    cr and dep versions update

--- spannerflow/rust_lib/build.rs
+++ spannerflow/rust_lib/build.rs
@@ -1,5 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rustc-link-search=all=/Users/ofer/code/technion/spannerflow/rust_server/target/debug/deps");
     tonic_build::compile_protos("../proto/dataflow/v1/dataflow.proto")?;
     Ok(())
 }
